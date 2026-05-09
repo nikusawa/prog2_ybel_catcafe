f4a58108fe788b4ae28bcb07ed25281009dad141
nikita <nikitatows@proton.me>
2026-05-09 10:49:58 +0200

    cat cafe test file added

---
+++ src/test/java/catcafe/CatCafeTest.java
@@ -0,0 +1,28 @@
+package catcafe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatCafeTest {
+
+    @Test
+    void addCat() {
+    }
+
+    @Test
+    void getCatCount() {
+    }
+
+    @Test
+    void getCatByName() {
+    }
+
+    @Test
+    void getCatByWeight() {
+    }
+
+    @Test
+    void accept() {
+    }
+}
